910243d02c130093be1bacd4551d50d1b371e2ed
miguelavanci <miguelavanci825@gmail.com>
2025-05-21 17:44:10 +0000

    aula 21/05

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express'); 
+
+const app = express(); 
+  
+app.set('view engine', 'hbs'); 
+ 
+app.set('views', './views'); 
+ 
+app.get('/', (request, response) => { 
+  let resultado = []; 
+
+  let a = 0, b = 1;
+  for (let cont = 1; cont <= 20; cont++) { 
+    resultado.push(a); 
+    [a, b] = [b, a + b];
+  } 
+
+  response.render('home', { valores: resultado }); 
+}); 
+
+app.listen(3000, () => { 
+  console.log('Servidor na porta 3000'); 
+});
